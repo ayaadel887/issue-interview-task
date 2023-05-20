3772fcf96238b4c7140010d74234d2db1275a8f7
ayaadel887 <aya.abdelgoad@gmail.com>
2023-05-21 02:30:59 +0300

    Add style to form callender

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
-
 import { departments, EmployeData, typeIssue, companyImg } from "./Data.js";
 import IssueTicket from "./Pages/IssueTicket/IssueTicket";
 function App() {
   return (
     <>
-      <IssueTicket
-        departments={departments}
-        EmployeData={EmployeData}
-        typeIssue={typeIssue}
-        company={companyImg}
-      />
+      <div className="App">
+        <IssueTicket
+          departments={departments}
+          EmployeData={EmployeData}
+          typeIssue={typeIssue}
+          company={companyImg}
+        />
+      </div>
     </>
   );
 }
