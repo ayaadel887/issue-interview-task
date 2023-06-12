183e4def4f765db6e96d2bc5b34fc35c2ec372ca
ayaadel887 <aya.abdelgoad@gmail.com>
2023-06-12 15:49:21 +0300

    Edit style in  people component

--- src/component/People/People.js
+++ src/component/People/People.js
@@ -6,8 +6,33 @@ const People = ({ EmployeData }) => {
     setSelectPeople(id);
   };
   return (
-    <div className={styles.container}>
-      <h2 className={styles.heder}>Direct it to whome ?</h2>
+    <>
+      <section className={styles.PeopleSection} id="testimonials">
+        <div className={styles.container}>
+          <div className={styles.boxes}>
+            {EmployeData.map((person) => (
+              <div
+                className={
+                  selectPeople === person.id ? styles.selectPeople : styles.box
+                }
+                onClick={() => handlerSelectPeople(person.id)}
+                key={person.id}
+              >
+                <img src={person.source} alt="" /> <h3>{person.name}</h3>
+                <span>Front-end Developer</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default People;
+{
+  /* <div className={styles.container}>
+       <h2 className={styles.heder}>Direct it to whome ?</h2>
       <div className={styles.content}>
         {EmployeData.map((person) => (
           <div
@@ -23,9 +48,5 @@ const People = ({ EmployeData }) => {
             <p className={styles.title}>{person.name}</p>
           </div>
         ))}
-      </div>
-    </div>
-  );
-};
-
-export default People;
+      </div> */
+}
